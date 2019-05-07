270c0083018f1ee3bbb5d43ec9af38c5864fdddd
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-05-07 16:23:54 -0400

    Updates

---
+++ precache-manifest.b02e91283a79325a5ed275a4cd87928c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0e6bd8037f773c7be4bd7163a4a57c",
+    "url": "/ozzy-clicky-game/index.html"
+  },
+  {
+    "revision": "e39f28544784db86a64e",
+    "url": "/ozzy-clicky-game/static/css/main.02c67017.chunk.css"
+  },
+  {
+    "revision": "7b72e14422e421864fa2",
+    "url": "/ozzy-clicky-game/static/js/2.9f88e8da.chunk.js"
+  },
+  {
+    "revision": "e39f28544784db86a64e",
+    "url": "/ozzy-clicky-game/static/js/main.cb44a7ec.chunk.js"
+  },
+  {
+    "revision": "f244410b19105279b895",
+    "url": "/ozzy-clicky-game/static/js/runtime~main.ae4eae49.js"
+  }
+]);
